d7a711ee07d4d0db05a85b7615d20b704d88ace1
Rajkumarp02 <rajkumar.p@techjays.com>
2024-09-06 17:00:46 +0530

    done

--- disenosys/next.config.mjs
+++ disenosys/next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
       },
+      sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
+      webpack: (config, { isServer }) => {
+        // Modify the webpack config as needed
+        return config;
+      },
 };
 
 export default nextConfig;
--- disenosys/postcss.config.mjs
+++ disenosys/postcss.config.mjs
@@ -3,6 +3,16 @@ const config = {
   plugins: {
     tailwindcss: {},
   },
+  parser: 'postcss-scss',
+  plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+      },
+    },
+  }
 };
 
 export default config;
