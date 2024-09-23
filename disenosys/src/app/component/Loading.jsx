95c0aff1d7a079202037a98c0c84fbd8f0c215e3
Rajkumarp02 <rajkumar.p@techjays.com>
2024-09-23 12:05:32 +0530

    changes spinner

--- disenosys/src/app/component/Loading.jsx
+++ disenosys/src/app/component/Loading.jsx
@@ -1,13 +1,19 @@
-"use client"
+"use client";
 
-import "../home/Home.css"
+import "../home/Home.css";
+import brand from "../assests/brand-1.png"; 
+import Image from "next/image";
+
 const LoadingSpinner = () => {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-100 bg-opacity-75 z-50 loader">
-        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    );
-  };
-  
-  export default LoadingSpinner;
-  
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-100 bg-opacity-75 z-50">
+      <Image
+        src={brand} 
+        alt="Loading..." 
+        className="w-24 h-24 border-4 border-[#182073] border-t-transparent rounded-full animate-spin" 
+      />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
