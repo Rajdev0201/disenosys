1d814114eefd96578d1babc8c92b99f6ea6933c1
Rajkumarp02 <rajkumar.p@techjays.com>
2024-09-27 13:17:05 +0530

    "update"

--- disenosys/src/app/auth/Glogout.jsx
+++ disenosys/src/app/auth/Glogout.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { GoogleLogout } from "@react-oauth/google";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
--- disenosys/src/app/login/page.js
+++ disenosys/src/app/login/page.js
@@ -1,12 +1,12 @@
-'use client';
+// 'use client';
 
-import { signIn } from 'next-auth/react';
-import React from 'react'
+// import { signIn } from 'next-auth/react';
+// import React from 'react'
 
-export default function Page() {
-  return (
-    <button onClick={() => signIn('linkedin')} className='mt-44'>
-    Sign in with LinkedIn
-    </button>
-  )
-}
+// export default function Page() {
+//   return (
+//     <button onClick={() => signIn('linkedin')} className='mt-44'>
+//     Sign in with LinkedIn
+//     </button>
+//   )
+// }
