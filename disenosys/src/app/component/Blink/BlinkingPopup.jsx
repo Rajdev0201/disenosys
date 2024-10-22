7dceb251e3376597c9e6c8ef8eefef021c1ad6b0
Rajkumarp02 <rajkumar.p@techjays.com>
2024-10-22 13:08:34 +0530

    ""

--- disenosys/src/app/component/Blink/BlinkingPopup.jsx
+++ disenosys/src/app/component/Blink/BlinkingPopup.jsx
@@ -9,7 +9,7 @@ const BlinkingAlert = () => {
     <div className="flex items-center justify-center space-x-4 bg-[#057FE3] text-center px-2 lg:px-0 py-2 lg:py-2">
     <div className="w-4 h-4 md:w-6 md:h-6 bg-[#182073] ring-1 lg:ring-2 ring-white shadow-xl rounded-full flex justify-center items-center">
     <div className="w-1 h-1 lg:w-2 lg:h-2 bg-red-500 animate-pulse rounded-full"></div>
-</div>
+    </div>
          <span className="font-semibold font-poppins text-xs lg:text-base text-white">
          Exciting changes ahead! Our basics are live—stay tuned for more!
       </span>
