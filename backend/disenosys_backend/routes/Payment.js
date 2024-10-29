2e7cb4899692a6d19639302be5a417e96aff276d
Rajkumarp02 <rajkumar.p@techjays.com>
2024-10-29 13:10:58 +0530

    "change email format"

--- backend/disenosys_backend/routes/Payment.js
+++ backend/disenosys_backend/routes/Payment.js
@@ -46,20 +46,26 @@ router.patch('/toggleCode/:id', async (req, res) => {
         to: updatedCode.customerDetails.email,
         subject: 'Your Course Has Been Activated!',
         html: `
-          <div style="font-family: Arial, sans-serif; color: #333;">
-      <div style="background-color: #f2f4f6; padding: 20px; text-align: center;">
-        <h1 style="color: #0d6efd; margin: 0;">Course Activation Notice</h1>
-      </div>
+    
+       <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; border: 1px solid #f0f0f0; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
+                <!-- Company Logo -->
+                <div style="background-color: #182073; padding: 20px; text-align: center; color: #fff;">
+                    <img src="https://www.disenosys.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.d25c986e.png&w=384&q=75" alt="Disenosys Logo" style="max-width: 150px; margin-bottom: 10px;">
+                    <h1 style="font-size: 24px; margin: 0;">Course Activation Notice</h1>
+                </div>
       <div style="background-color: #ffffff; padding: 20px; border-radius: 8px; max-width: 600px; margin: 20px auto; box-shadow: 0px 4px 12px rgba(0,0,0,0.1);">
         <h2 style="color: #333;">Dear ${updatedCode.customerDetails.name},</h2>
         <p style="font-size: 16px; color: #666;">We are pleased to inform you that your course has been <strong>activated</strong> by our admin team.</p>
-        
-        <div style="background-color: #f9f9f9; padding: 10px 20px; border-left: 4px solid #0d6efd; margin: 20px 0; border-radius: 4px;">
+       <p style="font-size: 16px; color: #666;">
+  Here you can find your course: 
+  <strong><a href="https://www.disenosys.com/mycourse" style="color: #0d6efd; text-decoration: none;">Visit Course</a></strong>
+</p>
+        <div style="background-color: #f9f9f9; padding: 10px 20px; border-left: 4px solid #182073; margin: 20px 0; border-radius: 4px;">
           <p style="font-size: 16px; margin: 0;"><strong>Session ID:</strong> ${updatedCode.sessionId}</p>
           <p style="font-size: 16px; margin: 0;"><strong>Status:</strong> Active</p>
         </div>
 
-         <table style="width: 100%; border-collapse: collapse; margin: 20px 0; background-color: #f9f9f9;">
+         <table style="width: 100%; border-collapse: collapse; margin: 20px 0; background-color: #182073;">
                     <thead>
                         <tr style="background-color: #4a90e2; color: #fff;">
                             <th style="padding: 10px; border: 1px solid #ddd; text-align: left;">Course Name</th>
