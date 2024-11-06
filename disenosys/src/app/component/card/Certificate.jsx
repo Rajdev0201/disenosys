80383cb7a294c7b477d1a69c5bce9d4bd39675e2
Rajkumarp02 <rajkumar.p@techjays.com>
2024-11-06 16:47:37 +0530

    :push

--- disenosys/src/app/component/card/Certificate.jsx
+++ disenosys/src/app/component/card/Certificate.jsx
@@ -8,7 +8,7 @@ import "../../globals.css";
 const CertificateComponent = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col  w-[1000px]  rounded relative overflow-hidden h-[1200px] bg-center hover:cursor-pointer c">
+      <div className="flex flex-col  w-[900px]  rounded relative overflow-hidden h-[1200px] bg-center hover:cursor-pointer c">
         <div className="ml-8 text-center flex flex-col items-center">
           <div className="w-64 h-24 mr-12 mb-16">
             <Image src={logo} className="text-blue-600" />
@@ -25,7 +25,7 @@ const CertificateComponent = () => {
           <p className="text-3xl font-berlin font-light text-gray-800 mr-16 mb-4 text-center">This certifies that</p>
           <h3 className="text-[#cc1919]  font-medium text-7xl font-sans ml-56 mb-4 text-center font-brush cname">Saim Wajid Qureshi</h3>
       </div>
-      <div className="mt-2 ml-16">
+      <div className="mt-2 ml-4">
       <p className="text-md mt-4 text-gray-700 ml-80 w-[580px] ">
         has completed the <span className="font-semibold">CATIA V5 Surface Design Internship</span> at Diseñosys from <span className="font-semibold">17th December 2023</span> to <span className="font-semibold">17th January 2024</span>.
       </p>
