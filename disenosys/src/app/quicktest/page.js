58f253694fd3fc39faf8d19727f832fc984fce7c
Rajkumarp02 <rajkumar.p@techjays.com>
2024-11-26 15:33:38 +0530

    "add"

--- disenosys/src/app/quicktest/page.js
+++ disenosys/src/app/quicktest/page.js
@@ -3,7 +3,23 @@ import Footer from "../component/Navbar/Footer";
 import Navbar from "../component/Navbar/Navbar";
 import Main from "./Main"
 
-
+const getImageUrl = (score) => {
+    return `https://www.disenosys.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fl.8f3043b1.jpg&w=3840&q=75`;
+  };
+  
+  export async function generateMetadata({ params, searchParams }) {
+    const catia = parseFloat(searchParams.catia) || 0;
+    const product = parseFloat(searchParams.product) || 0;
+    const yourScore = (catia + product) / 2;
+  
+    const metaDescription = `Your English proficiency score is ${yourScore}%. Take the challenge and improve your skills!`;
+  
+    return {
+      title: `Take the challenge and improve your skills!`,
+      description: metaDescription,
+      image: getImageUrl(yourScore),
+    };
+  }
 export default function QuickTestPage() {
 
     return(
--- disenosys/src/app/results/page.js
+++ disenosys/src/app/results/page.js
@@ -1,34 +1,6 @@
 import Results from "./Results";
 
-const getImageUrl = (score) => {
-    return `https://www.disenosys.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fl.8f3043b1.jpg&w=3840&q=75`;
-  };
-  
-  export async function generateMetadata({ searchParams }) {
-    const catia = parseFloat(searchParams.catia) || 0;
-    const product = parseFloat(searchParams.product) || 0;
-    const yourScore = (catia + product) / 2;
-  
-    const metaDescription = `Your English proficiency score is ${yourScore}%. Take the challenge and improve your skills!`;
-  
-    return {
-      title: `Your English Proficiency Score: ${yourScore}% , Take the challenge and improve your skills! | Disenosys`,
-      description: metaDescription,
-      openGraph: {
-        title: `Your English Proficiency Score: ${yourScore}% , Take the challenge and improve your skills! | Disenosys`,
-        description: metaDescription,
-        image: getImageUrl(yourScore),
-        url: `https://www.disenosys.com/quicktest?catia=${catia}&product=${product}&score=${yourScore}`,
-      },
-      twitter: {
-        card: "summary_large_image",
-        title: `Your English Proficiency Score: ${yourScore}% , Take the challenge and improve your skills! | Disenosys`,
-        description: metaDescription,
-        image: getImageUrl(yourScore),
-      },
-    };
-  }
-  
+
 
 export default function Page() {
   return (
