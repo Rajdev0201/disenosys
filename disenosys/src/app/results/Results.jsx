4c512ac8c39e16678a0880725226cd03a497cb5e
Rajkumarp02 <rajkumar.p@techjays.com>
2024-12-03 11:58:06 +0530

    "change logic"

--- disenosys/src/app/results/Results.jsx
+++ disenosys/src/app/results/Results.jsx
@@ -445,7 +445,7 @@ const Results = () => {
             </div>
           </div>
 
-          {showPopup && !accessToken && (
+          {showPopup && accessToken && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-lg p-8 max-w-sm w-full">
             <h2 className="text-2xl font-semibold text-gray-800 mb-4">
