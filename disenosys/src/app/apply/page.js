4a86d11a8597671b344249546e85cd5e971eeb16
Rajkumarp02 <rajkumar.p@techjays.com>
2025-08-06 12:04:01 +0530

    ""

--- disenosys/src/app/apply/page.js
+++ disenosys/src/app/apply/page.js
@@ -2,6 +2,15 @@ import Form from "./Form";
 
 
 
+export function generateMetadata () {
+    return {
+        title:"Application Form",
+        description: 'Disenosys is the preferred training and hiring partner for leading automotive OEMs and design studios. We equip mechanical engineers with real-world skills in BIW, Trims, Seating and more through industry-aligned programs that produce job-ready talent.'
+      }
+}
+
+
+
 export default function LeadsPage(){
     return(
         <div>
