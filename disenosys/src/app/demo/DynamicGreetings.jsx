b5285da86829ea102811dbb2b0489416f1a18cca
Rajkumarp02 <rajkumar.p@techjays.com>
2026-02-11 12:48:51 +0530

    ""

--- disenosys/src/app/demo/DynamicGreetings.jsx
+++ disenosys/src/app/demo/DynamicGreetings.jsx
@@ -1,50 +1,8 @@
 import React from 'react'
 
 const DynamicGreetings = () => {
-
-  const data = [
-    {
-        name:"riya",
-        age:"24",
-        city:"chennai",
-        skils:["mern"],
-        bio:{
-            caption:"im good at mern"
-        }
-    },
-     {
-        name:"ramya",
-        age:"22",
-        city:"chennai",
-        skils:["mern"],
-        bio:{
-            caption:"im good at mern"
-        }
-    },
-     {
-        name:"vino",
-        age:"21",
-        city:"chennai",
-        skils:["mern"],
-        bio:{
-            caption:"im good at mern"
-        }
-    },
-    ]
   return (
-    <div >
-     {
-      data.map((std,id) => (
-        <div className='' key={id}>
-           {std.skils.map((s) => (
-             <li>
-              {s}
-             </li>
-           ))}
-        </div>
-      ))
-     }
-    </div>
+    <div>DynamicGreetings</div>
   )
 }
 
